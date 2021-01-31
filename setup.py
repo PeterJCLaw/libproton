a5182fd5ad54a709fa72bcb2057283abd46f5c22
Peter Law <PeterJCLaw@gmail.com>
2021-01-31 16:05:17 +0000

    Add classifiers

--- setup.py
+++ setup.py
@@ -15,14 +15,32 @@ setup(
     description=description,
     author="Peter Law",
     author_email="PeterJCLaw@gmail.com",
+
     install_requires=[
         'PyYAML >=3.11, <5',
     ],
     license='MIT',
+
     tests_require=[
         'nose >=1.3, <2',
         'mock >=1.0.1, <2',
     ],
     test_suite='nose.collector',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+
     zip_safe=True,
 )
